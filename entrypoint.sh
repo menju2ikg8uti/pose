94105ac81078d3198d810c838c9e41b1bcbd55df
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-01 00:40:27 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
+set -e
 
-POOL_HOST=${POOL_HOST:-leywapool.com}
-POOL_PORT=${POOL_PORT:-6322}
-PROXY_PORT=${PROXY_PORT:-3334}
-
-echo "Starting Stratum Proxy..."
-echo "Pool: $POOL_HOST:$POOL_PORT"
-echo "Proxy Port: $PROXY_PORT"
-
-python mining_proxy.py \
-  -o $POOL_HOST \
-  -p $POOL_PORT \
-  -sh 0.0.0.0 \
-  -sp $PROXY_PORT
+echo "Starting tumPro..."
+echo "Pool: ew"
