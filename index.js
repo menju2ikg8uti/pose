9bfbe026a448c4b0effeb3ff6097e7ff55dbafab
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-01 00:48:16 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+import http from "http";
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({
+    status: "ok",
+    message: "Hello from simple Node.js 🚀"
+  }));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
